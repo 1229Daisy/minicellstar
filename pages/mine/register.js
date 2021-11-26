b8e8d8e0988accbc15ffbd882d9ba7d68b3fa8dd
liangxiaoyuan <8979@qq.com>
2021-11-26 16:29:32 +0800

    lxy

--- pages/mine/register.js
+++ pages/mine/register.js
@@ -7,7 +7,7 @@ Page({
     data: {
 
     },
-
+   
     /**
      * 生命周期函数--监听页面加载
      */
@@ -40,7 +40,10 @@ Page({
      * 生命周期函数--监听页面卸载
      */
     onUnload: function () {
-
+        wx.navigateBack({
+            delta: 1
+        })
+      
     },
 
     /**
